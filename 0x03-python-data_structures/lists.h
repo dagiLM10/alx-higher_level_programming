d0575272ff0a27611ede09762508d2d48404e58c
dagiLM10 <dagiberhanu19@gmail.com>
2023-08-14 16:42:11 +0000

    update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,13 +5,12 @@
 #include <stdlib.h>
 
 /**
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
+*struct listint_s - singly linked list
+*@n: integer
+*@next: points to the next node
 *
-* Description: singly linked list node structure
+*Description: singly linked list node structure
 */
-
 typedef struct listint_s
 {
 	int n;
