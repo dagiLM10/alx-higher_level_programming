a742c1d34860559f94b6bd6da90ceeb102aa40f8
dagiLM10 <dagiberhanu19@gmail.com>
2024-01-04 03:33:19 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__ == "__main__":
     import hidden_4
     # Print sorted name from directory
-    for names in sorted( dir(hidden_4)):
+    for name in sorted(dir(hidden_4)):
         # Print only names that do not start with __
        if name[:2] != '__':
           print("{}".format(name))
