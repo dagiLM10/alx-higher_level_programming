d58ae02614ec6562c8f354deaae5b12b7e85f6ef
dagiLM10 <dagiberhanu19@gmail.com>
2024-02-06 08:41:02 -0800

    updates

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Defines a file-appending function."""
 
 
-def append_write(file="", text=""):
+def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
 
     Args:
