bb550f426558a83d19fa68bd8c2c18b8cc1ea12f
dagiLM10 <dagiberhanu19@gmail.com>
2024-03-12 09:26:09 -0700

    updates

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,2 @@
--- List all privilages of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost':
-SHOW GRANTS FOR 'user_0d_2'@'localhost':
+-- Lists all databases of the my MySQL server.
+SHOW DATABASE;
