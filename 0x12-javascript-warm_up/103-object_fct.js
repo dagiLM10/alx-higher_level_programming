b25188872a71981e08b3cbf7e750c1b044c10f81
dagiLM10 <dagiberhanu19@gmail.com>
2024-04-08 09:05:57 -0700

    update

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myObject = {
+	type: 'Object',
+	value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
