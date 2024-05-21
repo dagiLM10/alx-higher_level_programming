83759667a6a322158d2acffac852a9ef1c547f3e
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 10:29:11 -0700

    update

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/`;
+
+request(apiUrl, function (error, response, body) {
+
+	if (!error && response.statusCode === 200) {
+
+		const movieData = JSON.parse(body);
+
+		const characterPromises = movieData.characters.map((characterUrl) => {
+			return new Promise((resolve, reject) => {
+				request(characterUrl, function (charError, charResponse, charBody) {
+					if (!charError && charResponse.statusCode === 200) {
+						const characterData = JSON.parse(charBody);
+					resolve(characterData.name);
+					} else {
+					reject(new Error(`Error fetching character data: ${charError}`));
+				}
+			});
+		});
+	});
+
+	Promise.all(characterPromises)
+          .then((characterNames) => {
+		console.log(characterNames.join('\n'));
+	  })
+	.catch((error) => {
+		console.error(error.message);
+	} else {
+	console.error('Error fetching movie data:', error);
+	}
+});
