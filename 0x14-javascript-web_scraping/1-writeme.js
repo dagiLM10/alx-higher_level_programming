cc75abae1012c2135f464723450f81d52284d683
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 04:19:59 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+//import the built-in Node.js 'fs' module.
 
-fs.write(process.argv[2], process.argv[3], 'utf-8',
-	function (err) {
-		if (err) {
-			console.log(err);
+fs.write(process.argv[2], process.argv[3], 'utf8', error => {
+//use fs.writeFile() to write data to a file specified as the third command-line argument
+		if (error) {
+		// if an error occurs during the write operation. the 'error' parameter will contain an error object
+			console.error(error);
 		}
 	});
