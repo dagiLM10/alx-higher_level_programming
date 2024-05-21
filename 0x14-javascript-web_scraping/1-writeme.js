2489de5aea5c89c565d3ba984ce65a94b234620b
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 08:37:34 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', error => {
 
       if (error) {
 	console.error(error);
