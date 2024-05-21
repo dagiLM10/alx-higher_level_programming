92256b1d80ef06cd3c2c59d17ac2a7d1f741fac6
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 04:12:15 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-//Read from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-	function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	});
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+
+       if (error) {
+	       console.error('Error reading the file:', error);
+
+                 } else {
+			 console.log(content);
+		 }
+});
