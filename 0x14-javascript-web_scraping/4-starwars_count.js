e8e98564df16ea170591bae2eed0aa2551b69fe3
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 09:20:48 -0700

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
+let num = 0;
 
-request(process.argv[2], function (error, response, body)  {
-	if (!error) {
-		const results = JSON.parse(body).result;
-		console.log(results.reduce((count, movie) => {
-			return movie.characters.find((character) => character.endsWith('/18/'))
-			? count + 1
-			: count;
-		}, 0));
+request.get(process.argv[2], (error, response, body) => {
+	if (error) {
+		console.log(error);
+	} else {
+		const content = JSON.parse(body);
+		content.results.forEach((film) => {
+			film.characters.forEach((character) => {
+				if (character.includes(18)) {
+					num += 1;
+				}
+			});
+		});
+		console.log(num);
 	}
 });
