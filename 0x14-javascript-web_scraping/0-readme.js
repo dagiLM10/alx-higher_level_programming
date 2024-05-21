877bdf3f32020fd61477dc0740454c4eb74f6771
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-21 08:34:20 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
 
 	if (error) {
 		console.error('Error reading the file:', error);
